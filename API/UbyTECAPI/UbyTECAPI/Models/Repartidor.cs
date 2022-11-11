feabfc3752f6d57ade191a9f9314e9b3613b5afa
PabloNaranjo78 <67924944+PabloNaranjo78@users.noreply.github.com>
2022-11-11 01:51:22 -0600

    Empleado, Repartidor y telefonosRepartidor listos

--- API/UbyTECAPI/UbyTECAPI/Models/Repartidor.cs
+++ API/UbyTECAPI/UbyTECAPI/Models/Repartidor.cs
@@ -1,9 +1,27 @@
-﻿namespace UbyTECAPI.Models
+﻿using Npgsql;
+
+namespace UbyTECAPI.Models
 {
-    public class Repartidor
+    public class Repartidor : Entity<Repartidor>
     {
+
+        protected override string? atributes
+        {
+            get => "usuario,pass,nombre,apellidos,disponible,provincia,canton,distrito";
+        }
+
+        protected override string? entity
+        {
+            get => "repartidor";
+        }
+
+        protected override string? searchAtribute
+        {
+            get => "usuario";
+        }
+
         public string? usuario { get; set; }
-        public string? password { get; set; }
+        public string? pass { get; set; }
         public string? nombre { get; set; }
         public string? apellidos { get; set; }
         public bool disponible { get; set; }
@@ -11,5 +29,34 @@
         public string? canton { get; set; }
         public string? distrito { get; set; }
 
+        protected override Repartidor createEntity(NpgsqlDataReader rd)
+        {
+            return new Repartidor
+            {
+                usuario = rd["usuario"].ToString(),
+                pass = rd["pass"].ToString(),
+                nombre = rd["nombre"].ToString(),
+                apellidos = rd["apellidos"].ToString(),
+                disponible = Boolean.Parse(rd["disponible"].ToString()),
+                provincia = rd["provincia"].ToString(),
+                canton = rd["canton"].ToString(),
+                distrito = rd["distrito"].ToString()
+            };
+        }
+        protected override string paramsToString()
+        {
+            return $"'{usuario}','{pass}','{nombre}','{apellidos}','{disponible}','{provincia}','{canton}','{distrito}'";
+        }
+
+        protected override string putParams()
+        {
+            return $"pass='{pass}',nombre='{nombre}',apellidos='{apellidos}',disponible='{disponible}',provincia='{provincia}',canton='{canton}',distrito='{distrito}'";
+        }
+
+        protected override string getID()
+        {
+            return $"'{usuario}'";
+        }
     }
+
 }
--- API/UbyTECAPI/UbyTECAPI/Models/RepartidorTelefonos.cs
+++ API/UbyTECAPI/UbyTECAPI/Models/RepartidorTelefonos.cs
@@ -1,8 +1,46 @@
-﻿namespace UbyTECAPI.Models
+﻿using Npgsql;
+
+namespace UbyTECAPI.Models
 {
-    public class RepartidorTelefonos
+    public class RepartidorTelefonos : Entity<RepartidorTelefonos>
     {
+        protected override string? atributes
+        {
+            get => "usuarioRep,telefono";
+        }
+
+        protected override string? entity
+        {
+            get => "repartidor_telefonos";
+        }
+
+        protected override string? searchAtribute
+        {
+            get => "usuarioRep";
+        }
         public string? usuarioRep { get; set; }
         public int telefono { get; set; }
+
+        protected override RepartidorTelefonos createEntity(NpgsqlDataReader rd)
+        {
+            return new RepartidorTelefonos
+            {
+                usuarioRep = rd["usuarioRep"].ToString(),
+                telefono = Convert.ToInt32(rd["telefono"])
+            };
+        }
+        protected override string paramsToString()
+        {
+            return $"'{usuarioRep}',{telefono}";
+        }
+
+        protected override string putParams()
+        {
+            return $"usuarioRep='{usuarioRep}',telefono={telefono}";
+        }
+        protected override string getID()
+        {
+            return $"'{usuarioRep}'";
+        }
     }
 }
