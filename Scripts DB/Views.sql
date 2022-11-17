895396385be73c705a4962002ad2b8670ec611e4
FMonge <fmonge13@estudiantec.cr>
2022-11-16 21:22:12 -0600

    Update Views.sql

--- Scripts DB/Views.sql
+++ Scripts DB/Views.sql
@@ -1,6 +1,5 @@
 CREATE VIEW Ventas_Afiliado 
 AS
-
 SELECT comercio.nombre AS Afiliado, count(pedido.idPedido) AS Compras, SUM(producto.precio * producto_pedido.cantidad) AS Monto_Total, SUM(producto.precio * producto_pedido.cantidad) * 0.05 AS Monto_Servicio
 FROM (((pedido JOIN producto_pedido  ON pedido.idPedido=producto_pedido.idPedido) 
 JOIN Producto ON producto=producto.nombre) 
@@ -8,32 +7,14 @@ JOIN Producto ON producto=producto.nombre)
 			   WHERE Pedido.finalizado = TRUE  GROUP BY comercio.idComercio
 
 
-
-
-Select * from Ventas_Afiliado;
-
-
-create or replace function totales3()
-    returns table
-            (
-                afiliado VARCHAR,
-				compras bigint, 
-				Monto_Total bigint,
-				Monto_Servicio bigint
-            )
-    language plpgsql
-as
-$$
-begin
-    return query (
-      SELECT comercio.nombre AS Afiliado, count(pedido.idPedido) AS Compras, SUM(producto.precio * producto_pedido.cantidad) AS Monto_Total, SUM(producto.precio * producto_pedido.cantidad) * 0.05 AS Monto_Servicio
-FROM (((pedido JOIN producto_pedido  ON pedido.idPedido=producto_pedido.idPedido) 
+CREATE VIEW Consolidado_Ventas
+AS
+SELECT Cliente.nombre AS Cliente, comercio.nombre AS Afiliado, count(pedido.idPedido) AS Compras, Pedido.repartidor AS Repartidor, SUM(producto.precio * producto_pedido.cantidad) AS Monto_Total, SUM(producto.precio * producto_pedido.cantidad) * 0.05 AS Monto_Servicio
+FROM ((((pedido JOIN producto_pedido  ON pedido.idPedido=producto_pedido.idPedido) 
 JOIN Producto ON producto=producto.nombre) 
-			   JOIN comercio ON producto.idComercio = comercio.idComercio )
-			   WHERE Pedido.finalizado = TRUE  GROUP BY comercio.idComercio
-    );
-end;
-$$;
+			   JOIN comercio ON producto.idComercio = comercio.idComercio ) JOIN Cliente ON pedido.idCliente = Cliente.idCliente)
+			   WHERE Pedido.finalizado = TRUE GROUP BY Pedido.idpedido, Comercio.nombre, Cliente.nombre
+			   ORDER BY Cliente.nombre ASC;
+			   
 
 
-Select totales3();
