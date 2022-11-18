7cf730a2c4af4d9ac82f0d999c32562dd56002ea
Axel Cordero <axeljcm@estudiantec.cr>
2022-11-18 17:27:22 -0600

    Reportes (falta solicitud)

--- ubyAdministracion/src/app/reportes/reportes.component.ts
+++ ubyAdministracion/src/app/reportes/reportes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Comercio } from '../interfaces/comercio';
-import { ComerciosService } from '../services/comercios.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Reporte, ReporteInterface } from '../interfaces/reporte';
+import { ReportesService } from '../services/reportes.service';
+
 
 @Component({
   selector: 'app-reportes',
@@ -9,18 +10,102 @@ import { ComerciosService } from '../services/comercios.service';
   styleUrls: ['./reportes.component.css']
 })
 export class ReportesComponent implements OnInit {
+  reporte1:Reporte[] = [];
+  reporte2:Reporte[] = [];
+  reporte3:Reporte[] = [];
+  listaclientes:Cliente[] = [];
+  listaafiliados:Afiliado[] = [];
 
-  constructor(private rou:ActivatedRoute, private comercioService:ComerciosService) { }
-  objeto:Comercio = new Comercio()
+
+  constructor(private rou:ActivatedRoute, private route:Router, private reportesService:ReportesService) { }
 
   ngOnInit(): void {
-    this.comercioService.get(this.rou.snapshot.params['comercio']).subscribe({
+    this.reportesService.get('ConsolidadoVentas').subscribe({
       next: data => {
-        this.objeto=data[0]
-        console.log(this.objeto)
+        this.reporte1=data;
+        this.sumreporte1();
+        console.log(this.reporte1)
       }
     })
+    this.reportesService.get('VentasAfiliado').subscribe({
+      next: data => {
+        this.reporte2=data
+        this.sumreporte2();
+        console.log(this.reporte2)
+      }
+    })
+  }
+  sumreporte1(){
+    let añadido:boolean = false;
+    for(let i = 0; i < this.reporte1.length; i++){
+      añadido = false;
+      for(let j = 0; j < this.listaclientes.length; j++){
+        if(this.reporte1[i].cliente == this.listaclientes[j].cliente){
+          this.listaclientes[j].añadirdetalle(this.reporte1[i]);
+          añadido = true;
+        }
+      }
+      if(!añadido){
+        this.listaclientes.push(new Cliente(this.reporte1[i].cliente, this.reporte1[i]))
+    }
+  }
+}
+  sumreporte2(){
+    let añadido2:boolean = false;
+    for (let i = 0; i < this.reporte2.length; i++) {
+      for(let j = 0; j < this.listaafiliados.length; j++){
+        if(this.reporte2[i].afiliado == this.listaafiliados[j].afiliado){
+          this.listaafiliados[j].añadirdetalle2(this.reporte2[i]);
+          añadido2 = true;
+        }
+      }
+      if(!añadido2){
+        this.listaafiliados.push(new Afiliado(this.reporte2[i].afiliado, this.reporte2[i]))
+    }
+  }
+}
+}
 
+
+class Cliente{
+  cliente!: string;
+  totalcomprasT:number = 0;
+  MontoTotalT:number = 0;
+  MontoServicioT:number = 0;
+
+  detalles:Reporte[]=[]
+
+  constructor(cliente:string, detalle:Reporte){
+    this.cliente = cliente;
+    this.añadirdetalle(detalle)
   }
 
+  añadirdetalle(detalle:Reporte){
+    this.totalcomprasT += detalle.compras;
+    this.MontoTotalT += detalle.monto_total;
+    this.MontoServicioT += detalle.monto_servicio;
+    this.detalles.push(detalle)
+  }
+}
+
+
+class Afiliado{
+  afiliado!: string;
+  totalcomprasS:number = 0;
+  MontoTotalS:number = 0;
+  MontoServicioS:number = 0;
+
+  detalles2:Reporte[]=[]
+
+  constructor(afiliado:string, detalle2:Reporte){
+    this.afiliado = afiliado;
+    this.añadirdetalle2(detalle2)
+  }
+
+  añadirdetalle2(detalle2:Reporte){
+    this.totalcomprasS += detalle2.compras;
+    this.MontoTotalS += detalle2.monto_total;
+    this.MontoServicioS += detalle2.monto_servicio;
+    this.detalles2.push(detalle2)
+  }
 }
--- ubyAdministracion/src/app/services/reportes.service.ts
+++ ubyAdministracion/src/app/services/reportes.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Gestion } from '../interfaces/gestion';
+import { ReporteInterface } from '../interfaces/reporte';
+import { ConexionService } from './conexion.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ReportesService {
+export class ReportesService extends ConexionService<ReporteInterface>{
+  listaG:Gestion[]=[]
+  getResourceURL(): string {
+    return "/Reportes"
+    //api repartidor
+  }
+  getHomePage(): string {
+    return 'gestion/reportes'
+  }
+  getNombre(): string {
+    return "Reporte"
+  }
 
-  constructor() { }
+  getAsInterface(): Gestion[]{
+    this.listaG=[];
+    this.getList().subscribe((data) =>{
+      for (let i =0; i<data.length;i++){
+        this.listaG.push(
+          {nombre: data[i].afiliado,
+            id: data[i].afiliado,
+            route:data[i].afiliado}
+          )
+      }
+    })
+    return this.listaG;
+  }
+
+  constructor(protected override httpClient: HttpClient, protected override route:Router) {
+    super(httpClient, route);
+  }
 }
