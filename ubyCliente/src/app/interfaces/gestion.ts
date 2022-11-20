d05f7cd740ffd06e08f86108c1d1cf465d809c2b
Axel Cordero <axeljcm@estudiantec.cr>
2022-11-19 18:43:19 -0600

    Login Cliente

---
+++ ubyCliente/src/app/interfaces/gestion.ts
@@ -0,0 +1,7 @@
+export interface Gestion
+{
+    nombre: number | string;
+    id: number | string;
+    route: string
+}
+
--- ubyCliente/src/app/services/clientes.service.ts
+++ ubyCliente/src/app/services/clientes.service.ts
@@ -1,9 +1,54 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { ClienteInterface } from '../interfaces/cliente';
+import { ComercioInterface } from '../interfaces/comercio';
+import { Gestion } from '../interfaces/gestion';
+import { ConexionService } from './conexion.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ClientesService {
+ export class ClientesService extends ConexionService<ClienteInterface>{
+    listaG:Gestion[]=[];
+    id!: number;
+    getResourceURL(): string {
+      return "/Clientes"
+    }
+    getHomePage(): string {
+      return this.id + '/gestion/perfil'
+    }
+    getNombre(): string {
+      return "Cliente"
+    }
 
-  constructor() { }
-}
+    getAsInterface(solicitud?:boolean): Gestion[]{
+      this.listaG = []
+      console.log(solicitud)
+      if (solicitud){
+        this.get("solicitudes").subscribe((data) =>{
+          for (let i =0; i<data.length;i++){
+            this.listaG.push(
+              {nombre: data[i].nombre,
+                id: data[i].idCliente,
+                route:data[i].nombre}
+              )
+          }
+        })
+      } else {
+      this.getList().subscribe((data) =>{
+        for (let i =0; i<data.length;i++){
+          this.listaG.push(
+            {nombre: data[i].nombre,
+              id: data[i].idCliente,
+              route:data[i].nombre}
+            )
+        }
+      })}
+      return this.listaG;
+    }
+
+    constructor(protected override httpClient: HttpClient, protected override route:Router) {
+      super(httpClient, route);
+    }
+  }
