b26acbe2512e4b550dc76c1284e579a80f89248d
Axel Cordero <axeljcm@estudiantec.cr>
2022-11-22 22:53:32 -0600

    Login css

--- ubyCliente/src/app/header/header.component.ts
+++ ubyCliente/src/app/header/header.component.ts
@@ -17,33 +17,33 @@ export class HeaderComponent implements OnInit {
     this.id = cookie.get("tokenCliente")
   }
 
-  recargar(Tipo:string){
-    this.router.navigate(['gestion/'+Tipo])
-    .then(() => {
-      window.location.reload();
-  });
+    recargar(Tipo:string){
+      this.router.navigate(['gestion/'+Tipo])
+      .then(() => {
+        window.location.reload();
+    });
 
-}
+  }
 
-  reloadComponent(self:boolean,urlToNavigateTo ?:string){
-    //skipLocationChange:true means dont update the url to / when navigating
-   console.log("Current route I am on:",this.router.url);
-   const url=self ? this.router.url :urlToNavigateTo;
-   this.router.navigateByUrl('/',{skipLocationChange:true}).then(()=>{
-     this.router.navigate([`/${url}`]).then(()=>{
-       console.log(`After navigation I am on:${this.router.url}`)
-     })
-   })
- }
+    reloadComponent(self:boolean,urlToNavigateTo ?:string){
+      //skipLocationChange:true means dont update the url to / when navigating
+    console.log("Current route I am on:",this.router.url);
+    const url=self ? this.router.url :urlToNavigateTo;
+    this.router.navigateByUrl('/',{skipLocationChange:true}).then(()=>{
+      this.router.navigate([`/${url}`]).then(()=>{
+        console.log(`After navigation I am on:${this.router.url}`)
+      })
+    })
+  }
 
- reloadPage(){
-   window.location.reload()
- }
- logout(){
-  this.cookie.delete("tokenCliente")
-  this.reloadPage()
-  console.log(this.cookie.get("tokenCliente"))
-}
+  reloadPage(){
+    window.location.reload()
+  }
+
+  logout(){
+    this.cookie.delete("tokenCliente")
+    this.reloadPage()
+  }
 
   ngOnInit(): void {
   }
